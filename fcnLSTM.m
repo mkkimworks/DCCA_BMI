97e485b3068287899a077c4084bc2900cc7452ff
mkkimworks <63139308+mkkimworks@users.noreply.github.com>
2020-09-09 14:34:51 +0900

    Delete fcnLSTM.m

--- fcnLSTM.m
+++
@@ -1,6 +0,0 @@
-function lstm_param         = fcnLSTM(X,Y, NumRepeats, lstm_param)
-
-if nargin < 4
-    [Net,~]                 = LSTM_Optimization(X, Y, NumRepeats, 'MonkeyF');
-    lstm_param              = Net.trainedNet;
-end
